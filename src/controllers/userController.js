e1c2e81b569008bb38aa4f82aad3353845153a23
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-07-29 11:55:11 +0600

    make variable for serach and pagination

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,8 @@ const User=require('../models/userModels')
 
   const getUser=async (req, res,next) => {
     try {
+      const search=req.query.search || ""
+      const page=Number(req.query.page) || 1
       const users= await User.find()
         res.status(200).send({message:'user is working',
         users
