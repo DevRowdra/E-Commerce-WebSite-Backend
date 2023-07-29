5e8b9176c2fb74ac9115cfa72288e2bd6ba7f3b9
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-07-29 11:51:23 +0600

    create get all user route for admin

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,11 @@
 const createError = require('http-errors');
+const User=require('../models/userModels')
 
-
-  const getUser= (req, res,next) => {
+  const getUser=async (req, res,next) => {
     try {
+      const users= await User.find()
         res.status(200).send({message:'user is working',
+        users
   });
     } catch (error) {
         next(error)
