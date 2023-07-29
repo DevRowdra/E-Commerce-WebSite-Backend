01e2eea15fb374ba13aa2204b09e1fefa96109c7
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-07-29 12:27:06 +0600

    add search functio work done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,28 @@
 const createError = require('http-errors');
-const User=require('../models/userModels')
+const User = require('../models/userModels');
 
-  const getUser=async (req, res,next) => {
-    try {
-      const search=req.query.search || ""
-      const page=Number(req.query.page) || 1
-      const users= await User.find()
-        res.status(200).send({message:'user is working',
-        users
-  });
-    } catch (error) {
-        next(error)
-    }
+const getUser = async (req, res, next) => {
+  try {
+    const search = req.query.search || '';
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 5;
+    // this regExp use for user name searching
+    const searchRegExp =new RegExp('.*'+ search +'.*', 'i');
+
+    // this filter use for show normal user not admin, $ne for not equel 
+    const filter = {
+      isAdmin: { $ne: true },
+      // here we filter or search using this regexp on name ,phone, email
+      $or:[
+        {name:{$regex:searchRegExp}},
+        {phone:{$regex:searchRegExp}},
+        {email:{$regex:searchRegExp}},
+      ]
+    };
+    const users = await User.find(filter);
+    res.status(200).send({ message: 'user is working', users });
+  } catch (error) {
+    next(error);
   }
-  module.exports={getUser}
+};
+module.exports = { getUser };
