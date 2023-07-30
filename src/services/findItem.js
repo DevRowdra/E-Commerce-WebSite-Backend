989cee499de9089e9365fca836b6ca9fc4b1eaaf
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-07-30 10:24:22 +0600

    make dynamic findItem function

--- src/services/findItem.js
+++ src/services/findItem.js
@@ -1,13 +1,12 @@
 const createHttpError = require("http-errors");
-const User = require("../models/userModels");
 const { default: mongoose } = require("mongoose");
 
-const findWithId=async(id,options={})=>{
+const findWithId=async(Model,id,options={})=>{
    try {
-    const item = await User.findById(id, options)
+    const item = await Model.findById(id, options)
    
   
-    if (!item) {throw createHttpError(404, 'item dose not found with this email ')};
+    if (!item) {throw createHttpError(404, `${Model.modelName} dose not found with this email `)};
     return item
    } catch (error) {
     if(error instanceof mongoose.Error){
