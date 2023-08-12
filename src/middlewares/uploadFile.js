251b55a0921ba798c1b37a5d6be7f96d4c208118
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-08-12 18:56:40 +0600

    make file size and types workable

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -1,8 +1,10 @@
 const multer=require('multer')
 const path=require('path')
+const createError = require('http-errors');
+
 const UPLOAD_DIR=process.env.UPLOAD_FILE || 'public/images/users';
-const MAX_FILE_SIZE=process.env.MAX_FILE_SIZE || 2097152;
-const ALLOWED_FILE_TYPES=process.env.ALLOWED_FILE_TYPES || ['jpg','jpeg','png'];
+const MAX_FILE_SIZE=Number(process.env.MAX_FILE_SIZE) || 2097152;
+const ALLOWED_FILE_TYPES=process.env.ALLOWED_FILE_TYPES || ['jpeg','png'];
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, UPLOAD_DIR)
@@ -12,6 +14,19 @@ const storage = multer.diskStorage({
       cb(null, Date.now()+ '-' + file.originalname.replace(extname,'')+extname)
     }
   })
+  const fileFilter=(req,file,cb)=>{
+    const extname=path.extname(file.originalname)
+    if (!ALLOWED_FILE_TYPES.includes(extname.substring(1))) {
+    const error=  createError(400,'File type not allowed')
+    return cb(error)
+    }
+    cb(null,true)
+  }
+  const upload = multer({ storage: storage 
+  ,
+  limits:{fileSize:MAX_FILE_SIZE},
+  fileFilter,
   
-  const upload = multer({ storage: storage })
+  
+  })
   module.exports=upload;
